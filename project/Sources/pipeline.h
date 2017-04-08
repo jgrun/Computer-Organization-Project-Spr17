af00bc7205e1e2ed0e5d1248cc92d9e54fd20863
Jacob Grunwald <jacob.grunwald@colorado.edu>
2017-04-08 16:16:38 -0600

    Added in some pipeline definitions and the beginning of the fetch function

--- project/Sources/pipeline.h
+++ project/Sources/pipeline.h
@@ -8,7 +8,24 @@
 #ifndef PIPELINE_H_
 #define PIPELINE_H_
 
-void instruction_fetch();
+#define OP_MASK 0xfc000000
+#define RS_MASK 0x03e00000
+#define RT_MASK 0x001f0000
+#define RD_MASK 0x0000f800
+#define SH_MASK 0x000007c0
+#define FC_MASK 0x0000003f
+#define AD_MASK 0x03ffffff
+#define IM_MASK 0x0000ffff
+
+#define SHIFT_OP 26
+#define SHIFT_RS 21
+#define SHIFT_RT 16
+#define SHIFT_RD 11
+#define SHIFR_SH 6
+
+
+
+void instruction_fetch(control ifid, pc_t pc);
 
 void instruction_decode();
 
@@ -17,5 +34,7 @@ void execute_instruction();
 void memory_access();
 
 void write_back();
+
+
 
 #endif /* PIPELINE_H_ */
